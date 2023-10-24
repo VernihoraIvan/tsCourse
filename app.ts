7643863f3e96fe28c90d65f9910dd9abf7606f77
VernihoraIvan <vernihorai@gmail.com>
2023-10-24 12:15:35 +0200

    upd

--- app.ts
+++ app.ts
@@ -9,3 +9,15 @@ function add(num1: number, num2: number) {
 button.addEventListener("click", function () {
   console.log(add(+input1.value, +input2.value));
 });
+
+function calc(
+  param1: number,
+  param2: number,
+  callback: (num1: number, num2: number) => number
+): void {
+  console.log("Result:", callback(param1, param2));
+}
+
+calc(1, 1, (num1, num2) => num1 + num2);
+
+calc(10, 5, (num1, num2) => num1 - num2);
